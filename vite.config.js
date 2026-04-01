c4a7c6199c336c4909b31ba16df3103e6c8be2bd
ProjectMaker1 <giokika@gmail.com>
2026-04-01 22:02:52 +0400

    fix for local

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,5 @@ const ADMIN_DEV = "http://localhost:3000"; // ან 3001 — სადაც �
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/admin": {
-        target: ADMIN_DEV,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (p) => p.replace(/^\/admin/, ""),
-      },
-      "/static": { target: ADMIN_DEV, changeOrigin: true, secure: false },
-      "/asset-manifest.json": { target: ADMIN_DEV, changeOrigin: true, secure: false },
-      "/manifest.json": { target: ADMIN_DEV, changeOrigin: true, secure: false },
-      "/sockjs-node": { target: ADMIN_DEV, changeOrigin: true, secure: false, ws: true },
-    },
-  },
+
 });
